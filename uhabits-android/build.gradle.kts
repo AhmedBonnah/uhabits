0d6e4018cf5eba159d835e0f0cb8e5541da05eae
RiNoLaptop <admin@rinoo.net>
2026-06-22 18:52:19 +0300

    Bump version to 2.3.4

--- uhabits-android/build.gradle.kts
+++ uhabits-android/build.gradle.kts
@@ -44,8 +44,8 @@ android {
     compileSdk = 36
 
     defaultConfig {
-        versionCode = 20303
-        versionName = "2.3.3"
+        versionCode = 20304
+        versionName = "2.3.4"
         minSdk = 28
         targetSdk = 36
         applicationId = "org.isoron.uhabits"
